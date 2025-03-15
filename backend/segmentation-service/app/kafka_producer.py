6362ed591493a2af52e8fc0f6b58a92b7d6755f6
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2025-03-15 19:09:25 +0100

    FIx: passing start_altitude and end_altitude to segments. Closes #126

--- backend/segmentation-service/app/kafka_producer.py
+++ backend/segmentation-service/app/kafka_producer.py
@@ -24,24 +24,12 @@ producer = KafkaProducer(
 
 def prepare_segmentation_message(activity_id, segments_df, processed_at, start_date):
     """Prepare the output message with compressed segments"""
-    segments = []
-    
-    for idx, row in segments_df.iterrows():
-        segment = {
-            "segmentId": f"{activity_id}-{idx+1}",  # create unique ID for each segment
-            "start_distance": row["start_distance"],
-            "end_distance": row["end_distance"],
-            "segment_length": row["segment_length"],
-            "avg_gradient": row["avg_gradient"],
-            "avg_cadence": row["avg_cadence"],
-            "type": row["type"],
-            "grade_category": row["grade_category"],
-            "start_lat": row["start_lat"],
-            "start_lng": row["start_lng"],
-            "end_lat": row["end_lat"],
-            "end_lng": row["end_lng"]
-        }
-        segments.append(segment)
+    segments_df = segments_df.assign(
+        segmentId=segments_df.index.to_series().add(1).astype(str).radd(f"{activity_id}-")
+    )
+
+    # Convert the dataframe into a dictionary of lists of dictionaries
+    segments = segments_df.to_dict(orient="records")
 
     # Convert segments in json and compress
     json_segments = json.dumps(segments).encode("utf-8")
@@ -58,7 +46,6 @@ def prepare_segmentation_message(activity_id, segments_df, processed_at, start_d
 
 def send_segmentation_output(activity_id, segments_df, processed_at, start_date):
     """Send segmentation output message to Kafka."""
-    
     # Prepare message with compressed segments
     kafka_message = prepare_segmentation_message(activity_id, segments_df, processed_at, start_date)
 
