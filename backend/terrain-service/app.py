baeb357ebc546b8b3296fda260b28cb11616b217
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2025-03-18 18:08:08 +0100

    Fix Logger

--- backend/terrain-service/app.py
+++ backend/terrain-service/app.py
@@ -2,7 +2,7 @@ import logging_setup
 import logging
 from app.kafka_consumer import start_kafka_consumer
 
-logger = logging.getLogger("terrain")
+logger = logging.getLogger("app")
 logger.info("Terrain service started successfully")
 
 
