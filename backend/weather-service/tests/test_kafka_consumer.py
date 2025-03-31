05ecdce992ffd657561697682ffc6da3dcdec315
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2025-03-31 12:10:26 +0200

    Handle retry and simplify weather output. #134, #129

--- backend/weather-service/tests/test_kafka_consumer.py
+++ backend/weather-service/tests/test_kafka_consumer.py
@@ -22,28 +22,31 @@ def load_test_message(request):
 
     return mock_message
 
-# @pytest.mark.parametrize('load_test_message', ['mock_segments_message.json'], indirect=True)
-# def test_process_message(load_test_message):
-#     """Tests process_message with a real Kafka message. It produces real output"""
-#     with patch('app.counter_manager.RequestCounter.increment', autospec=True) as mock_increment:
-#         process_message(load_test_message)
+@pytest.mark.parametrize('load_test_message', ['mock_segments_message.json'], indirect=True)
+def test_process_message(load_test_message):
+    """Tests process_message with a real Kafka message. It produces real output"""
+    with patch('app.weather_service.send_weather_output') as mock_send_output:
+        process_message(load_test_message)
+    # Assertions
+    assert mock_send_output.call_count == 6
 
-# @pytest.mark.parametrize('load_test_message', ['mock_retry_message.json'], indirect=True)
-# def test_process_retry_message(load_test_message):
-#     """Tests process_retry_message with a real Kafka message. It produces real output"""
+@pytest.mark.parametrize('load_test_message', ['mock_retry_message.json'], indirect=True)
+def test_process_retry_message(load_test_message):
+    """Tests process_retry_message with a real Kafka message. It produces real output"""
     
-#     # Mock the current time to simulate a specific timestamp
-#     # Let's say we mock the current time to be 10 seconds before retry_timestamp
-#     mock_current_time = 1743416199  # 10 seconds before retry_timestamp (1743416209)
+    # Mock the current time to simulate a specific timestamp
+    # Let's say we mock the current time to be 10 seconds before retry_timestamp
+    mock_current_time = 1743416199  # 10 seconds before retry_timestamp (1743416209)
 
     
-#     with patch('time.sleep') as mock_sleep:  # Mocking time.sleep to avoid actual waiting
-#         with patch('app.kafka_consumer.datetime') as mock_datetime:  # Mocking datetime
-#             # Mock datetime.now() to return a specific UTC timestamp when called
-#             mock_datetime.now.return_value = datetime.fromtimestamp(mock_current_time, tz=timezone.utc)
+    with patch('time.sleep') as mock_sleep:  # Mocking time.sleep to avoid actual waiting
+        with patch('app.kafka_consumer.datetime') as mock_datetime:  # Mocking datetime
+            # Mock datetime.now() to return a specific UTC timestamp when called
+            mock_datetime.now.return_value = datetime.fromtimestamp(mock_current_time, tz=timezone.utc)
 
-#             with patch('app.counter_manager.RequestCounter.increment', autospec=True) as mock_increment:    
-#                 # Run the function with the mocked time
-#                 process_retry_message(load_test_message)
-
+            with patch('app.weather_service.send_retry_message') as mock_send_retry:    
+                # Run the function with the mocked time
+                process_retry_message(load_test_message)
+        # Assertions        
+        mock_send_retry.assert_called_once
         
